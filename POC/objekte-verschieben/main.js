abda4b5af696f5f357c5717d27ed2ad63a47afe2
MKellner <maximilian_elias.kellner@smail.th-koeln.de>
2025-12-30 17:18:16 +0100

    Neues Positon/offset handling getestet #9

--- POC/objekte-verschieben/main.js
+++ POC/objekte-verschieben/main.js
@@ -11,12 +11,39 @@ const klemmbaustein = {
     // Editor aktivieren
     editMode: true,
 };
+const sportwagen = {
+    sceneConfigPath: '/car/scene-config.json', // Pfad zur Szenenkonfiguration
+    modelPath: '/car/911.glb', // Pfad zum explodierbaren .glb Modell
+    //modelPath: '/car/911-with-ground.glb', // Pfad zum explodierbaren .glb Modell
+    explosionConfigPath: '/car/911-exp-config.json', // Pfad zur Explosions-Konfiguration
+    cardDataPath: '/car/911-cards.json', // Pfad zu den Card-Inhalten
+    showDebugUI: true,
+    showStats: true,
+
+    editMode: true,  // Edit-Mode beim Start
+    
+    sceneConfig: {
+        backgroundColor: "#6480be", //old: 353535 -- blue: 6480be
+        camera: {
+            position: [5, 3, 9],
+            maxDistance: 50,
+            lockVertical: false,
+        }
+    },
+    
+    highlightOptions: {
+        highlightComponent: true,
+        mode: "ghost"  // 'wireframe' oder 'ghost'
+    },
+
+    infoElementType: 'card', // 'pointer', 'attached-card', 'card'
+}
 
 async function main() {
     const container = document.getElementById('exp-container');
     if (!container) return;
 
-    const viewer = new ExplodedViewer(container, klemmbaustein);
+    const viewer = new ExplodedViewer(container, sportwagen);
     await viewer.init();
 
     window.addEventListener('ev:objectSelected', (event) => {
