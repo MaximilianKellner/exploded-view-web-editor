d4777ae8d1f1c11b2842e71e41cd2c3281c46218
MKellner <maximilian_elias.kellner@smail.th-koeln.de>
2026-02-20 15:35:34 +0100

    Vorbereitung playground

--- Prototyp/vite.config.deploy.js
+++ Prototyp/vite.config.deploy.js
@@ -9,7 +9,8 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: './index.html',
+        playground: './pages/playground/index.html',
       }
     }
   }
